1a87bb72b330e2cd44ec7fa8670a2d710c467737
Ivan Lisyushkin <gagarincity@gmail.com>
2020-08-24 12:24:00 +0300

    Add Task1 solution

---
+++ src/day1/Task6.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 10; i++) {
+            int k = 2;
+            int result = i * k;
+            System.out.printf("%d x %d = %d \n", i, k, result);
+        }
+    }
+}
