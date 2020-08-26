5b57466e5eb6c13c471f2e3de3ed39a0c465d39a
Ivan Lisyushkin <gagarincity@gmail.com>
2020-08-26 13:09:43 +0300

    Add Task3 solution

--- src/day2/Task4.java
+++ src/day2/Task4.java
@@ -6,13 +6,14 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
+        double y = 0;
         if (x >= 5) {
-            double y = (x * x - 10) / (x + 7);
-            System.out.println(y);
+            y = (x * x - 10) / (x + 7);
         } else if (x > -3 && x < 5) {
-            double y = (x + 3) * (x * x - 2);
-            System.out.println(y);
-        } else
-            System.out.println(420);
+            y = (x + 3) * (x * x - 2);
+        } else {
+            y = 420;
+        }
+        System.out.println(y);
     }
 }
---
+++ src/day3/Task3.java
@@ -0,0 +1,22 @@
+package day3;
+
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int i = 0;
+        while (i < 5) {
+            double a = scanner.nextDouble();
+            double b = scanner.nextDouble();
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                i++;
+                continue;
+            }
+            System.out.println(a / b);
+            i++;
+        }
+    }
+}
