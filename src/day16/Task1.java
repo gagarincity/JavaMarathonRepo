e0b75083143a3b7c604771e347e692f1f8854167
Ivan Lisyushkin <gagarincity@gmail.com>
2020-09-09 17:58:14 +0300

    Add Task16 solution

---
+++ src/day16/Task1.java
@@ -0,0 +1,27 @@
+package day16;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("Task1Numbers");
+
+        Scanner sc = new Scanner(file);
+        String line = sc.nextLine();
+        String[] numbers = line.split(" ");
+
+        int sum = 0;
+        for (String number : numbers)
+            sum += Integer.parseInt(number);
+
+        double srArr = (double) sum / numbers.length;
+        System.out.print(srArr);
+        System.out.print(" --> ");
+        System.out.printf("%.3f", srArr);
+        sc.close();
+    }
+}
---
+++ src/day16/Task2.java
@@ -0,0 +1,44 @@
+package day16;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        Random random = new Random();
+        File file1 = new File("day16File1");
+        FileWriter fw1 = new FileWriter(file1);
+        FileWriter fw2 = new FileWriter("day16File2");
+
+        int[] numbers = new int[100];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = random.nextInt(1000);
+            fw1.write(numbers[i] + " ");
+        }
+        fw1.close();
+        int startIndex = 0;
+        int endIndex = 20;
+        int sum = 0;
+        double averageValue;
+
+        List<Double> numbersList = new ArrayList<>();
+        for (int i = startIndex; i < endIndex; i++) {
+            sum += numbers[i];
+            averageValue = (double) sum / 20;
+            numbersList.add(averageValue);
+            startIndex += 20;
+            endIndex += 20;
+            if (endIndex > 1000)
+                break;
+        }
+        fw2.write(numbersList + " ");
+        fw2.close();
+        double sum2 = 0;
+        for (int i = 0; i < 50; i++) {
+            sum2 += numbersList.get(i);
+        }
+        System.out.println((int) sum2);
+    }
+}
